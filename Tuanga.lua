f93ea10c0ee95c59fbc1fdefdf92343874ea0845
smilegaming050-droid <smilegaming050@gmail.com>
2026-03-17 18:07:07 +0700

    Update Tuanga.lua

--- Tuanga.lua
+++ Tuanga.lua
@@ -1,123 +1,266 @@
- getgenv().Config = {
-        Dashboard = {
-            Enabled = false, -- Connect to Adopt Me dashboard To Control Script/View Stats (https://zekehub.com/dashboard/adoptme)
-            GroupName = "vps1", -- Group name for organizing accounts on dashboard
-        },
-        BabyFarm = false, -- Does baby farm
-        PetFarm = {
-            Enabled = false, -- Enables the Pet Farm
-            FarmEggs = false, -- If true, equips eggs to hatch them. If false, equips regular pets
-            BuyEggs = false, -- If FarmEggs is true and no eggs in inventory, buy eggs automatically
-            EggTypes = {}, -- Which eggs to equip ({} = any egg, or {"cracked_egg", "royal_egg"} for specific)
-            BuyEggType = "any", -- Which egg to buy when BuyEggs is true ("any" or specific egg ID)
-            MaxPets = 1, -- How many pets to equip at once (1 = free, 2 = requires Robux gamepass)
-            FarmUntilFullGrown = false, -- If true, selects pets that aren't full grown first
-            PrioritizeFriendship = false, -- If true, selects pets with higher friendship level first
-            SelectiveFarm = false, -- If true, only farm pets in SelecatedPetTypes list
-            SelectedPetTypes = {}, -- Pet IDs to farm when SelectiveFarm is true (e.g., {"dog", "cat"})
-        },
-        EventFarm = {
-            CandyCliff = true, -- Automatically complete Candy Cliff event tasks and buys a chissle
-        },
-        AutoTrade = {
-            Enabled = false, -- Enable auto trading
-            AutoAcceptTrades = false, -- Accept incoming trade requests
-            AutoLeaveAfterTrades = false, -- Leave the game after completing trades
-            Usernames = {}, -- Players to send trades to
-            TradeMode = "all", -- "all" = everything in categories, "specific" = only Items list
-            Categories = {}, -- {"pets", "toys", "food", "transport", "gifts", "stickers", "pet_accessories", "roleplay"}
-            Items = {}, -- Item IDs when TradeMode = "specific"
-            ItemCounts = {}, -- Max count per item in Items array
-            PetTypes = {}, -- {} = all, {"regular", "neon", "mega"}
-            Ages = {}, -- {} = all, {1, 2, 3, 4, 5, 6}
-        },
-        AutoNeon = {
-            Enabled = false, -- Enable auto neon/mega fusion
-            MakeMega = false, -- Fuse neons into mega neons
-            NeonAll = true, -- Neon everything possible
-            SelectedPets = {}, -- {} when NeonAll = true, otherwise {"dog", "cat"} etc
-            MaxPerType = {}, -- {} = unlimited, {dog = 2, cat = 1} = limits per pet type
-        },
-        AutoPotion = {
-            Enabled = false, -- Use age potions on pets to level them up
-            SelectedPets = {"lny_2026_fire_foal"}, -- Pet IDs to use potions on (empty = does nothing)
-        },
-        AutoBuy = {
-            Enabled = false, -- Automatically buy items from shops
-            SelectedItems = {}, -- Item IDs to buy
-            BuyAmounts = {}, -- How many of each item to buy. Example: {5, 10} buys 5 cracked_eggs and 10 sandwiches. Empty {} buys infinite of each item. If there are more items than amounts, remaining items default to infinite.
-        },
-        AutoPay = {
-            Enabled = false, -- Send bucks to another player
-            TargetPlayer = "", -- Username of player to pay bucks to
-        },
-        AutoOpen = {
-            Enabled = false, -- Open gift boxes, baits, etc automatically
-            Items = {}, -- Item IDs to auto open
-        },
-        AutoRecycle = {
-            Enabled = false, -- Toggle auto recycling on/off
-            RarityFilter = {
-                -- Each rarity maps to a list of versions to recycle
-                -- Versions: "regular", "neon", "mega"
-                -- If a rarity is not listed or empty, pets of that rarity will NOT be recycled
-                -- If a rarity has versions listed, ONLY those versions will be recycled
+task.spawn(function()
+    while true do
+        pcall(function()
+            settings().Rendering.FrameRateManager = Enum.FramerateManagerMode.Automatic
+            setfpscap(14)
+        end)
+        task.wait(5)
+    end
+end)
+spawn(function()
+    _G.Config = { 
+        UserID = "3f3fb26b-c3b5-4cd2-b7e3-4f8bba14fe23",
+        discord_id = "765845940074053633" ,
+        Note = "SAB", } 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/yummytrack/main/tracker"))()
+end)
+getgenv().Key = ""
+getgenv().Config = {
+    ["Gameplay"] = {
+        ["Server Type"] = "Private (Clone)",
 
-                -- common = {"regular", "neon", "mega"},  -- Recycle all common versions
-                -- uncommon = {"neon"},                    -- Only recycle neon uncommons
-                -- rare = {"regular", "neon", "mega"},     -- Recycle all rare versions
-                -- ultra_rare = {"regular", "neon", "mega"}, -- Recycle all ultra rare versions
-                -- legendary = {"mega"},                   -- Only recycle mega legendaries
+        -- Private Server config
+        ["Collect Cash Cap"] = "1000B",
+
+        -- Public Server config
+        ["Max Auctioning Multiplier"] = 10,
+        ["Lock Base Extra Time"] = 5,
+    },
+
+    ["Misc"] = {
+        ["Kick if FPS below"] = 1,
+        ["Kick if Ping above"] = 100000,
+        ["Max Rebirth"] = 9,
+        ["Ignore Secret"] = {
+            ["La Vacca Saturno Saturnita"] = { "All" },
+            ["Karkerkar Kurkur"] = { "All" },
+            ["Sammyni Spyderini"] = { "All" },
+            ["Extinct Tralalero"] = { "All" },
+            ["Extinct Matteo"] = { "All" },
+            ["Dul Dul Dul"] = { "All" },
+            ["Chachechi"] = { "All" },
+            ["Los Tralaleritos"] = { "All" },
+            ["Las Tralaleritas"] = { "All" },
+            ["La Cucaracha"] = { "All" },
+            ["Job Job Job Sahur"] = { "All" },
+            ["Frankentteo"] = { "All" },
+            ["Graipuss Medussi"] = { "All" },
+            ["Zombie Tralala"] = { "All" },
+            ["Perrito Burrito"] = { "All" },
+            ["To to to Sahur"] = { "All" },
+            ["Guerriro Digitale"] = { "All" },
+            ["Coffin Tung Tung Tung Sahur"] = { "All" },
+            ["Nooo My Hotspot"] = { "All" },
+            ["La Grande Combinasion"] = { "All" },
+            ["Chicleteira Bicicleteira"] = { "All" },
+            ["Chimnino"] = { "All" },
+            ["Bunnyman"] = { "All" },
+            ["Please my Present"] = { "All" },
+            ["Reindeer Tralala"] = { "All" },
+            ["La Vacca Prese Presente"] = { "All" },
+            ["Ho Ho Ho Sahur"] = { "All" },
+            ["List List List Sahur"] = { "All" },
+            ["Bisonte Giuppitere"] = { "All" },
+            ["Blackhole Goat"] = { "All" },
+            ["Chicleteirina Bicicleteirina"] = { "All" },
+            ["Chimpanzini Spiderini"] = { "All" },
+            ["Cuadramat and Pakrahmatmamat"] = { "All" },
+            ["Donkeyturbo Express"] = { "All" },
+            ["Giftini Spyderini"] = { "All" },
+            ["Horegini Boom"] = { "All" },
+            ["Jackorilla"] = { "All" },
+            ["Jolly Jolly Sahur"] = { "All" },
+            ["Karker Sahur"] = { "All" },
+            ["La Vacca Jacko Linterino"] = { "All" },
+            ["Los Chicleteiras"] = { "All" },
+            ["Los Cucarachas"] = { "All" },
+            ["Los Hotspotsitos"] = { "All" },
+            ["Los Jobcitos"] = { "All" },
+            ["Los Karkeritos"] = { "All" },
+            ["Los Matteos"] = { "All" },
+            ["Los Mobilis"] = { "All" },
+            ["Los Nooo My Hotspotsitos"] = { "All" },
+            ["Los Planitos"] = { "All" },
+            ["Los Quesadillas"] = { "All" },
+            ["Los Spyderinis"] = { "All" },
+            ["Los Tortus"] = { "All" },
+            ["Naughty Naughty"] = { "All" },
+            ["Noo my Candy"] = { "All" },
+            ["Noo my examine"] = { "All" },
+            ["Noo my Present"] = { "All" },
+            ["Pirulitoita Bicicleteira"] = { "All" },
+            ["Pot Hotspot"] = { "All" },
+            ["Pot Pumpkin"] = { "All" },
+            ["Pumpkini Spyderini"] = { "All" },
+            ["Quesadilla Crocodila"] = { "All" },
+            ["Rang Ring Bus"] = { "All" },
+            ["Santteo"] = { "All" },
+            ["Santa Hotspot"] = { "All" },
+            ["Torrtuginni Dragonfrutini"] = { "All" },
+            ["Trickolino"] = { "All" },
+            ["Triplito Tralaleritos"] = { "All" },
+            ["Tung Tung Tung Sahur"] = { "All" },
+            ["Vulturino Skeletono"] = { "All" },
+            ["Yess my examine"] = { "All" },
+            ["GOAT"] = { "All" },
+            ["Brunito Marsito"] = {"All"},
+            ["Chill Puppy"] = {"All"},
+            ["Los Trios"] = {"All"},
+	    ["Mi Gatitos"] = {"All"},
+	    ["Luv Luv Luv"] = {"All"},
+	    ["Karkerheart Luvkur"] = {"All"},
+	    ["Love Love Love Sahur"] = {"All"},
+	    ["Cupid Hotspot"] = {"All"},
+	    ["Noo my Heart"] = {"All"},
+	    ["Chicleteira Cupideira"] = {"All"},
+	    ["Cupid Cupid Sahur"] = {"All"},
+            ["Fishboard"] = {"All"},
+            ["Nuclearo Dinossauro"] = {"All"},
+            ["DJ Panda"] = {"All"},
+        },
+
+        ["Auto Fuse"] = {
+            ["Enable"] = false,
+            ["Fuse Animal"] = {
+                ["La Vacca Saturno Saturnita"] = { "All" },
+                ["Karkerkar Kurkur"] = { "All" },
+                ["Sammyni Spyderini"] = { "All" },
+                ["Extinct Tralalero"] = { "All" },
+                ["Extinct Matteo"] = { "All" },
+                ["Dul Dul Dul"] = { "All" },
+                ["Chachechi"] = { "All" },
+                ["Los Tralaleritos"] = { "All" },
+                ["Las Tralaleritas"] = { "All" },
+                ["La Cucaracha"] = { "All" },
+                ["Job Job Job Sahur"] = { "All" },
+                ["Frankentteo"] = { "All" },
+                ["Graipuss Medussi"] = { "All" },
+                ["Zombie Tralala"] = { "All" },
+                ["Perrito Burrito"] = { "All" },
+                ["To to to Sahur"] = { "All" },
+                ["Guerriro Digitale"] = { "All" },
+                ["Coffin Tung Tung Tung Sahur"] = { "All" },
+                ["Nooo My Hotspot"] = { "All" },
+                ["La Grande Combinasion"] = { "All" },
+                ["Chicleteira Bicicleteira"] = { "All" },
+                ["Chimnino"] = { "All" },
+                ["Bunnyman"] = { "All" },
+                ["Please my Present"] = { "All" },
+                ["Reindeer Tralala"] = { "All" },
+                ["La Vacca Prese Presente"] = { "All" },
+                ["Ho Ho Ho Sahur"] = { "All" },
+                ["List List List Sahur"] = { "All" },
+                ["Bisonte Giuppitere"] = { "All" },
+                ["Blackhole Goat"] = { "All" },
+                ["Chicleteirina Bicicleteirina"] = { "All" },
+                ["Chimpanzini Spiderini"] = { "All" },
+                ["Cuadramat and Pakrahmatmamat"] = { "All" },
+                ["Donkeyturbo Express"] = { "All" },
+                ["Giftini Spyderini"] = { "All" },
+                ["Horegini Boom"] = { "All" },
+                ["Jackorilla"] = { "All" },
+                ["Jolly Jolly Sahur"] = { "All" },
+                ["Karker Sahur"] = { "All" },
+                ["La Vacca Jacko Linterino"] = { "All" },
+                ["Los Chicleteiras"] = { "All" },
+                ["Los Cucarachas"] = { "All" },
+                ["Los Hotspotsitos"] = { "All" },
+                ["Los Jobcitos"] = { "All" },
+                ["Los Karkeritos"] = { "All" },
+                ["Los Matteos"] = { "All" },
+                ["Los Mobilis"] = { "All" },
+                ["Los Nooo My Hotspotsitos"] = { "All" },
+                ["Los Planitos"] = { "All" },
+                ["Los Quesadillas"] = { "All" },
+                ["Los Spyderinis"] = { "All" },
+                ["Los Tortus"] = { "All" },
+                ["Naughty Naughty"] = { "All" },
+                ["Noo my Candy"] = { "All" },
+                ["Noo my examine"] = { "All" },
+                ["Noo my Present"] = { "All" },
+                ["Pirulitoita Bicicleteira"] = { "All" },
+                ["Pot Hotspot"] = { "All" },
+                ["Pot Pumpkin"] = { "All" },
+                ["Pumpkini Spyderini"] = { "All" },
+                ["Quesadilla Crocodila"] = { "All" },
+                ["Rang Ring Bus"] = { "All" },
+                ["Santteo"] = { "All" },
+                ["Santa Hotspot"] = { "All" },
+                ["Torrtuginni Dragonfrutini"] = { "All" },
+                ["Trickolino"] = { "All" },
+                ["Triplito Tralaleritos"] = { "All" },
+                ["Tung Tung Tung Sahur"] = { "All" },
+                ["Vulturino Skeletono"] = { "All" },
+                ["Yess my examine"] = { "All" },
+                ["GOAT"] = { "All" },
+                ["Brunito Marsito"] = {"All"},
+                ["Chill Puppy"] = {"All"},
+                ["Los Trios"] = {"All"},
+	    	["Mi Gatitos"] = {"All"},
+	        ["Luv Luv Luv"] = {"All"},
+	    	["Karkerheart Luvkur"] = {"All"},
+	    	["Love Love Love Sahur"] = {"All"},
+	    	["Cupid Hotspot"] = {"All"},
+	    	["Noo my Heart"] = {"All"},
+	        ["Chicleteira Cupideira"] = {"All"},
+		["Cupid Cupid Sahur"] = {"All"},
+                ["Fishboard"] = {"All"},
+                ["Nuclearo Dinossauro"] = {"All"},
+                ["DJ Panda"] = {"All"},
             },
-            AgeFilter = {}, -- Empty = all ages, or specific ages e.g. {1, 2, 3, 4, 5, 6} (1=Newborn, 6=Full Grown)
-            ExcludedPets = {}, -- Pet IDs to never recycle e.g. {"dog", "cat", "shadow_dragon"}
         },
-        IdleProgression = {
-            Enabled = false, -- Put pets in pet pen for idle leveling
-            SelectedPets = {"cracked_egg"}, -- Pet IDs to put in pet pen (empty = use all)
-            ExcludedPets = {}, -- Pet IDs to never put in pet pen
-            PriorityOrder = {}, -- Order: first = highest priority for pen slots (e.g. {"neon", "regular", "mega"})
+
+        ["Rare Animal Generation"] = "20M",
+
+        ["Open Lucky Block"] = {
+            "Spooky Lucky Block",
+            "Festive Lucky Block",
+            "Spooky Lucky Block",
+            "Heart Lucky Block",
+            "Taco Lucky Block",
+            "Los Lucky Blocks",
+            "Admin Lucky Block",
+            "Los Taco Blocks",
+
         },
-        AccountManager = {
-            Enabled = false, -- Master toggle for account management
-            Tool = "", -- "yummy", "farmsync"
-            Yummy = {
-                Action = "completed", -- "completed" = remove cookie/stop, "swap" = next cookie
-                Reason = "Done", -- Suffix for completed file (Completed-{Reason})
-            },
-            FarmSync = {
-                Action = "completed", -- "completed" = move to done folder, "swap" = move and replace
-                FromFolderId = "", -- Fresh cookies folder ID
-                ToFolderId = "", -- Done cookies folder ID
-                ChangeWithoutReplacement = false, -- Remove even if no replacement available
-                ConfigId = nil, -- Config for new account (nil = same config)
-            },
-            Triggers = {
-                AfterTradeComplete = false, -- Requires AutoTrade.AutoLeaveAfterTrades to be enabled
-                MinBucks = 0, -- Change account when bucks >= this (0 = off)
-                MinPotions = 0, -- Change account when potions >= this (0 = off)
-            },
+
+        ["Buy Lucky Block"] = {
+	    "Heart Lucky Block",
+            "Secret Lucky Block",
+            "Festive Lucky Block",
+            "Spooky Lucky Block",
+            "Taco Lucky Block",
+            "Los Lucky Blocks",
+            "Admin Lucky Block",
+            "Los Taco Blocks",
+
         },
-        Settings = {
-            AutoShowUI = true, -- Load the UI on script start (main overlay disable for less memory usage)
-            ShowOverlay = false, -- Show stats overlay (disables 3D rendering)
-            ReduceGraphics = false, -- Reduce graphics quality to minimum
-            FPSCap = 0, -- FPS cap option (0 = uncapped)
-            LureId = "ice_dimension_2025_ice_soup_bait" -- what lure to use for example: "ice_dimension_2025_ice_soup_bait"
-        },
-        Webhook = {
-            Enabled = false, -- Send webhook notifications to Discord
-            URL = "https://discord.com/api/", -- Discord webhook URL for notifications
-            PetUnlock = {
-                Enabled = false, -- Send webhook when hatching/unlocking a pet
-                URL = "https://discord.com/api/webhooks/", -- Webhook URL for pet unlocks
-                FilterRarities = {"legendary", "ultra_rare"}, -- Only send for these rarities
-            },
-        },
-        TaskExclusion = {
-            Enabled = false, -- Skip certain farming tasks
-            ExcludedTasks = {}, -- Task IDs to skip (e.g., {"buccaneer_band", "summerfest_bonfire"})
-        },
-    };
-getgenv().scriptkey="SSWtpoXTepWxxmYVhmhXVOlcbwjdSAfr"
-loadstring(game:HttpGet("https://zekehub.com/scripts/AdoptMe/MassFarm.lua"))()
+    },
+
+    ["Performance"] = {
+        ["Boost FPS"] = true,
+        ["Black Screen"] = true,
+    },
+
+    ["Webhook"] = {
+        ["Enable"] = true,
+        ["Url"] = "https://discord.com/api/webhooks/1420694714423705640/k6Bf2AaSaHHScKSyH0HMgGxbRxszIFwBs5BZ3QmI8wINHi2VF8n3xV342ITrQuk1zESE",
+        ["Ignore Notify"] = {},
+    },
+}
+
+spawn(function()
+    task.wait(30)  -- Change the wait time to 60 seconds (1 minute)
+    if not getgenv().scriptLoaded then
+        game.Players.LocalPlayer:Kick("Script load timeout\nKick to prevent animal gets stolen")
+    end
+end)
+
+-- Keep loading until Joebiden flag is set
+repeat
+    wait()
+    spawn(function()
+        loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
+    end)
+    wait(20)
+until getgenv().Joebiden
