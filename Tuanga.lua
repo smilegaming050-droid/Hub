d344f25d07083699012282c183c648408f07d15b
smilegaming050-droid <smilegaming050@gmail.com>
2026-05-06 23:06:18 +0700

    Update Tuanga.lua

--- Tuanga.lua
+++ Tuanga.lua
@@ -8,10 +8,10 @@ getgenv().UserConfig = {
         -- ["Bambu Sahur"] = { "Rainbow", "Plasma" },
         -- "Ketupat Kepat",
     },
-    ["Webhook URL"] = "",
+    ["Webhook URL"] = "https://discord.com/api/webhooks/1365662642563321906/l4vSLJ3o-WEpoGF0a9w-WVbzLOQKn74P9kXVRJnJ3XXy4Q2DqDElQqtYl8f23YIYQw7h",
     ["Webhook Brainrot Name"] = {}, 
     ["Webhook Rarity"] = { "OG", "Exclusive", "Celestial" },
-    ["Webhook Note"] = "",
-    ["Discord ID"] = "",
+    ["Webhook Note"] = "Kick",
+    ["Discord ID"] = "765845940074053633",
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/11e48947c5101db41ac846b20eaeb5ce.lua"))()
