928137a3d27fe8fa6abffc9200fb888643437614
smilegaming050-droid <smilegaming050@gmail.com>
2026-05-10 17:20:24 +0700

    Update Tuanga.lua

--- Tuanga.lua
+++ Tuanga.lua
@@ -1,17 +1,8 @@
-setfpscap(10) -- ghi fps vo nha
-script_key = "eWFyRNEvzKElQinNBYTjrpJIZEOixLsZ";
+script_key = "";
 getgenv().UserConfig = {
-    ["Max Rebirth"] = 5,
-    ["Min Kick Power"] = "1b", -- 1b or 2b
-    ["NOT SELL RARITY"] = {"Exclusive", "Celestial" },
-    ["NOT SELL PET"] = {
-        -- ["Bambu Sahur"] = { "Rainbow", "Plasma" },
-        -- "Ketupat Kepat",
-    },
-    ["Webhook URL"] = "https://discord.com/api/webhooks/1365662642563321906/l4vSLJ3o-WEpoGF0a9w-WVbzLOQKn74P9kXVRJnJ3XXy4Q2DqDElQqtYl8f23YIYQw7h",
-    ["Webhook Brainrot Name"] = {"Kick"}, 
-    ["Webhook Rarity"] = { "OG", "Exclusive", "Celestial" },
-    ["Webhook Note"] = "https://discord.com/api/webhooks/1365662642563321906/l4vSLJ3o-WEpoGF0a9w-WVbzLOQKn74P9kXVRJnJ3XXy4Q2DqDElQqtYl8f23YIYQw7h",
+    ["Webhook URL"] = "https://discord.com/api/webhooks/1420694714423705640/k6Bf2AaSaHHScKSyH0HMgGxbRxszIFwBs5BZ3QmI8wINHi2VF8n3xV342ITrQuk1zESE",
+    ["Webhook Ping"] = {"Huge", "Titanic", "Gargantuan", "Normal"}, -- xoa normal de khong spam webhook
+    ["Webhook Note"] = "Pet99",
     ["Discord ID"] = "765845940074053633",
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/11e48947c5101db41ac846b20eaeb5ce.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a569c6d231092c9a6d71d6da15e689a0.lua"))()
